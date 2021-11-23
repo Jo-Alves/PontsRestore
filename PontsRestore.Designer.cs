4f299fdff70c7ec6321ed83b64f0d6e26c32fc62
jo-alves <joelmirogerio@gmail.com>
2021-11-23 17:46:33 -0300

    first commit

---
+++ PontsRestore.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace PontsRestore
+{
+    partial class PontsRestore
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PontsRestore));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.lblOpenDirectory = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.colFile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFullPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Diretório";
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDirectory.Location = new System.Drawing.Point(49, 55);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(521, 26);
+            this.txtDirectory.TabIndex = 1;
+            // 
+            // dgvFiles
+            // 
+            this.dgvFiles.AllowUserToAddRows = false;
+            this.dgvFiles.AllowUserToDeleteRows = false;
+            this.dgvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvFiles.BackgroundColor = System.Drawing.Color.White;
+            this.dgvFiles.ColumnHeadersHeight = 30;
+            this.dgvFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colFile,
+            this.colDate,
+            this.colFullPath});
+            this.dgvFiles.Location = new System.Drawing.Point(24, 100);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.ReadOnly = true;
+            this.dgvFiles.RowHeadersVisible = false;
+            this.dgvFiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFiles.Size = new System.Drawing.Size(577, 260);
+            this.dgvFiles.TabIndex = 3;
+            this.dgvFiles.TabStop = false;
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestore.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnRestore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestore.Image = global::PontsRestore.Properties.Resources.Backup_restore_icon_24px;
+            this.btnRestore.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRestore.Location = new System.Drawing.Point(440, 376);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(161, 37);
+            this.btnRestore.TabIndex = 4;
+            this.btnRestore.Text = "Restaurar";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            // 
+            // lblOpenDirectory
+            // 
+            this.lblOpenDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblOpenDirectory.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblOpenDirectory.Image = global::PontsRestore.Properties.Resources.Mimetypes_inode_directory_icon_24px;
+            this.lblOpenDirectory.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblOpenDirectory.Location = new System.Drawing.Point(576, 55);
+            this.lblOpenDirectory.Name = "lblOpenDirectory";
+            this.lblOpenDirectory.Size = new System.Drawing.Size(34, 26);
+            this.lblOpenDirectory.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.lblOpenDirectory, "Abrir Diretório");
+            this.lblOpenDirectory.Click += new System.EventHandler(this.lblOpenDirectory_Click_1);
+            // 
+            // colFile
+            // 
+            this.colFile.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colFile.HeaderText = "Arquivo";
+            this.colFile.Name = "colFile";
+            this.colFile.ReadOnly = true;
+            this.colFile.Width = 86;
+            // 
+            // colDate
+            // 
+            this.colDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colDate.HeaderText = "Data de criação";
+            this.colDate.Name = "colDate";
+            this.colDate.ReadOnly = true;
+            this.colDate.Width = 145;
+            // 
+            // colFullPath
+            // 
+            this.colFullPath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colFullPath.HeaderText = "Caminho completo";
+            this.colFullPath.Name = "colFullPath";
+            this.colFullPath.ReadOnly = true;
+            // 
+            // PontsRestore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(627, 425);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.dgvFiles);
+            this.Controls.Add(this.lblOpenDirectory);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PontsRestore";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pontos de Restauração";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Label lblOpenDirectory;
+        private System.Windows.Forms.DataGridView dgvFiles;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFullPath;
+    }
+}
+
---
+++ PontsRestore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PontsRestore
+{
+    public partial class PontsRestore : Form
+    {
+        public PontsRestore()
+        {
+            InitializeComponent();
+        }
+
+        private void lblOpenDirectory_Click_1(object sender, EventArgs e)
+        {
+            FileInfo fi = new FileInfo(@"C:\Caixa Fácil\Imagens do sistema\Sem título.jpg");
+
+            txtDirectory.Text = fi.FullName.ToString();
+        }
+    }
+}
